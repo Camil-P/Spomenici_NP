29d3d62a9bc5974f3f737e1639c3c5fd26f8d6be
Camil-P <56632088+Camil-P@users.noreply.github.com>
2021-03-12 16:45:25 +0100

    Icon for browser tab

--- Spomenici_Client/main.js
+++ Spomenici_Client/main.js
@@ -64,7 +64,7 @@ function loadSights() {
   xhttp.send('json');
 }
 
-loadSights();
+// loadSights();
 
 // Making a Leaflet map ------------------------------------------------------------------------------------
 
